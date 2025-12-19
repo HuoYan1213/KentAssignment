d713de357646f205c928ed5d5a270922f22e37e7
HuoYan <huoyan0928@gmail.com>
2025-12-19 20:33:33 +0800

    Version (1.0.0)

--- app/db.php
+++ app/db.php
@@ -1,9 +1,16 @@
 <?php
+$host = 'food-ordering-db.c2pbvsssbp8t.us-east-1.rds.amazonaws.com';
+$dbname = 'ass';
+$user = 'admin';
+$pass = '12345678';
+
 try {
-        $_db = new PDO('mysql:dbname=ass;host=food-ordering-db.c2pbvsssbp8t.us-east-1.rds.amazonaws.com;charset=utf8mb4', 'admin', '12345678', [
+    $_db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+        PDO::ATTR_TIMEOUT => 5
     ]);
+
 } catch (PDOException $e) {
-    die('database connect fail: ' . $e->getMessage());
+    die("<h1 style='color:red'>數據庫連接炸了: " . $e->getMessage() . "</h1>");
 }
